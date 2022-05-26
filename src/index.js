5fb12d4f876ed30207ce846cdbde44b523487923
annnacaesar <Anechka22334@gmail.com>
2022-05-26 15:56:24 +0300

    в процесі

--- src/index.js
+++ src/index.js
@@ -1,1 +1,64 @@
 import './sass/main.scss';
+import { Notify } from 'notiflix';
+import ApiService from './js/api';
+import LoadMoreBtn from './js/load-more'
+import imageCard from './templates/image-card.hbs'
+
+const formRef = document.querySelector('#search-form');
+const galleryRef = document.querySelector('.gallery');
+
+
+
+const apiService = new ApiService();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '.load-more',
+  hidden: true,
+});
+
+formRef.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchImages);
+
+function onSearch (event) {
+	event.preventDefault();
+
+	apiService.searchQuery = event.currentTarget.elements.searchQuery.value.trim();
+
+	if (apiService.searchQuery === ''){
+		return Notify.failure('Please, enter text')
+	}
+
+	loadMoreBtn.show();
+	apiService.reset();
+	clearImagesContainer();
+	fetchImages();
+
+
+}
+
+function fetchImages () {
+	loadMoreBtn.disable();
+	apiService.fetchImage().then(images => {
+		console.log(apiService.page);
+	
+
+		if(apiService.page === 2){
+			Notify.info(`Hooray! We found ${apiService.totalHits} images.`)
+		}
+
+		if(images.length === 0){
+			Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+		} 
+
+		appendArticlesMarkup(images);
+		loadMoreBtn.enable();
+	});
+}
+
+function appendArticlesMarkup(images) {
+  galleryRef.insertAdjacentHTML('beforeend', imageCard(images));
+}
+
+function clearImagesContainer() {
+  galleryRef.innerHTML = '';
+}
+
---
+++ src/js/api.js
@@ -0,0 +1,37 @@
+export default class ApiService {
+	constructor() {
+		this.searchQuery = '';
+		this.page = 1;
+		this.per_page = 40;
+		this.totalHits = null;
+	}
+
+	fetchImage() {
+	const URL = `https://pixabay.com/api/?key=27616591-ea38afb15630e129e17ab0ac4&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.per_page}&page=${this.page}`;
+
+		return fetch(URL).then(response => {
+			if (!response.ok) {
+				throw new Error('Error fetching data');
+			}
+			return response.json();
+		}).then(({hits, totalHits}) => {
+			this.incrementPage();
+			console.log(this.totalHits);
+			this.totalHits = totalHits;
+			console.log(this.totalHits);
+			return hits;
+		})
+	}
+	
+	incrementPage() {
+    this.page += 1;
+  }
+
+  reset() {
+    this.page = 1;
+		this.totalHits = null;
+
+  }
+}
+
+// ?fields=webformatURL,largeImageURL,tags,likes,views,comments,downloads
